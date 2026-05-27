163947c43c3c3a7e3ccb0d33bf308ad21fbe1994
NHK <123547351+KietNH-ai@users.noreply.github.com>
2026-05-27 15:35:48 +0700

    Feat02 sql churn rate by segment (#13)
    * feat: add SQL churn rate by segment query
    
    * feat: add SQL churn rate by segment query
    
    * refactor: remove calculation logic
    
    * repush
    
    ---------
    
    Co-authored-by: Huỳnh Gia Bảo <huynhgiabao2718@gmail.com>

--- sql/churn_rate_by_segment.sql
+++ sql/churn_rate_by_segment.sql
@@ -1,5 +1,7 @@
 --Viết truy vấn SQL tính Churn Rate (`sql/churn_rate_by_segment.sql`) theo Loại hợp đồng, Phương thức thanh toán, và các gói dịch vụ đi kèm.-- sql/churn_rate_by_segment.sql
 SELECT
+    ID,
+    Churn,
     Contract,
     PaymentMethod,
     InternetService,
@@ -7,3 +9,5 @@ SELECT
     OnlineBackup,
     DeviceProtection,
     TechSupport
+FROM dbo.Predictive_Customer_Churn_DB;
+
