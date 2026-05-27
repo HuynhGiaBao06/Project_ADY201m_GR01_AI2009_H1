f1da464ec7efd9da9df313b2ff36fe48db9d808e
Huỳnh Gia Bảo <huynhgiabao2718@gmail.com>
2026-05-27 13:54:00 +0700

    Delete database directory

--- database/churn_rate_by_segment.sql
+++
@@ -1,9 +0,0 @@
---Viết truy vấn SQL tính Churn Rate (`sql/churn_rate_by_segment.sql`) theo Loại hợp đồng, Phương thức thanh toán, và các gói dịch vụ đi kèm.-- sql/churn_rate_by_segment.sql
-SELECT
-    Contract,
-    PaymentMethod,
-    InternetService,
-    OnlineSecurity,
-    OnlineBackup,
-    DeviceProtection,
-    TechSupport
